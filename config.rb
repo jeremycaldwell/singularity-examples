8b705f02763db259c306251ffe04fbb95787de8b
Jeremy Caldwell <jcaldwell@magnersanborn.com>
2013-10-18 12:54:47 -0700

    Added nested in nested columns

--- config.rb
+++ config.rb
@@ -1,13 +1,15 @@
 require 'singularitygs'
 # Require any additional compass plugins here.
 
+require 'breakpoint'
+require 'toolkit'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
